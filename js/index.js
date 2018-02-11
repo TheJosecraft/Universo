e65535c0ca37ef403a5fd27052a672a94bb9df01
Jose Carlos Raya Leon <josekawa1996@gmail.com>
2018-02-11 21:38:25 +0100

    Limpieza

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       solarsys = $("#solar-system");
 
   var init = function() {
-    body.removeClass('view-2D opening').addClass("view-3D").delay(2000).queue(function() {
+    body.removeClass('opening').delay(2000).queue(function() {
       $(this).removeClass('hide-UI').addClass("set-speed");
       $(this).dequeue();
     });
@@ -31,7 +31,7 @@ $(document).ready(function(){
     e.preventDefault();
   });
 
-  $(".set-view").click(function() { body.toggleClass("view-3D view-2D"); });
+  // $(".set-view").click(function() { body.toggleClass("view-3D view-2D"); });
   $(".set-zoom").click(function() { body.toggleClass("zoom-large zoom-close"); });
   $(".set-speed").click(function() { setView("scale-stretched set-speed"); });
   $(".set-size").click(function() { setView("scale-s set-size"); });
